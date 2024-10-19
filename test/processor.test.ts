2c5cc62af54d493eeaedcf04af97e04dabe0e50b
andrei <angpeu@gmail.com>
2024-10-20 01:41:51 +0300

    lesson 6 walked through

--- test/processor.test.ts
+++ test/processor.test.ts
@@ -29,4 +29,3 @@ test('check that consent is not approved  when user has not given consent', (): 
     //const processor = new Processor()
     expect(processor.verifyConsent(user)).toBeFalsy()
 })
-//done
